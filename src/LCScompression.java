97a448268e02848261201f1cd5d9bece807a398f
Peter Jang <peterj@bu.edu>
2017-11-01 17:10:47 -0400

    compress method now works

--- src/LCScompression.java
+++ src/LCScompression.java
@@ -11,21 +11,25 @@ public class LCScompression{
     }
 
     public String compress(String lcs, String file){
-        int i = 0;
-        while( i < file.length()){
+        while(file.contains(lcs)){
             int index=file.indexOf(lcs);//index of the longest common substring
             this.indexes.add(index);//add lcs index into the list of indexes
-            file.replaceFirst(lcs, "");//replace first lcs in the string with "" empty string
-            if (!file.contains(lcs)){
-                i = file.length() + 1;
-            }
+            file = file.replaceFirst(lcs, "");//replace first lcs in the string with "" empty string
+        }
+        for (int i = 0; i < indexes.size(); i++) {
+        		System.out.println(indexes.get(i));
         }
         this.compressed = file;
         return this.compressed;
     }
 
     public static void main(String args[]) {
-
+    	
+    		LCScompression L = new LCScompression();
+    		String file_test = "hellomynameisesenhellomynameispeterhellomynameisjosh";
+    		String lcs_test = "myname";
+    		
+    		System.out.println(L.compress(lcs_test, file_test));
 
     }
 
